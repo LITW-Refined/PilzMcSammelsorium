8a3d03729be248e693f7f4a06945b5bcf047be19
Pilzinsel64 <pilzinsel64@gmx.de>
2023-04-06 16:42:13 +0200

    spotlessApply

--- src/main/java/de/pilz/sammelsorium/RegisterManagement.java
+++ src/main/java/de/pilz/sammelsorium/RegisterManagement.java
@@ -3,6 +3,7 @@ package de.pilz.sammelsorium;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+
 import cpw.mods.fml.common.registry.GameRegistry;
 import de.pilz.sammelsorium.blocks.*;
 import de.pilz.sammelsorium.items.ItemCropsRedstone;
