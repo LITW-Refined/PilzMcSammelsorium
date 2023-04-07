5b71ce7961e41381e2c9809e3bf68adeed9511f9
Pilzinsel64 <pilzinsel64@gmx.de>
2023-04-07 14:48:58 +0200

    recipes for redstone flowers

--- src/main/java/de/pilz/sammelsorium/RecipesManagement.java
+++ src/main/java/de/pilz/sammelsorium/RecipesManagement.java
@@ -1,6 +1,7 @@
 package de.pilz.sammelsorium;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -9,11 +10,19 @@ public class RecipesManagement {
 
     public static void RegisterAllRecipes() {
         var torch = Blocks.torch; // Oder auch so: GameRegistry.findItem("minecraft", "torch");
+
         GameRegistry.addShapedRecipe(
                 new ItemStack(RegisterManagement.blockSeaLantern),
                 new Object[] { "F F", " F ", "F F", 'F', torch });
         GameRegistry.addShapedRecipe(
                 new ItemStack(RegisterManagement.blockSeaLantern, 1, 1),
                 new Object[] { "FFF", "   ", "FFF", 'F', torch });
+        
+        GameRegistry.addShapelessRecipe(
+                new ItemStack(RegisterManagement.itemCropsRedstone, 2, 0),
+                new Object[] { RegisterManagement.itemFlowerRedstone, RegisterManagement.itemFlowerRedstone });
+        GameRegistry.addShapelessRecipe(
+                new ItemStack(Items.redstone, 4, 0),
+                new Object[] { RegisterManagement.itemFlowerRedstone, RegisterManagement.itemFlowerRedstone, RegisterManagement.itemFlowerRedstone });
     }
 }
