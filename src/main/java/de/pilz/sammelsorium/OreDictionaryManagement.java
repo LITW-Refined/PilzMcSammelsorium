a667c3a9f312971aa091d1d948564f766e7efe43
Pilzinsel64 <pilzinsel64@gmx.de>
2023-04-10 20:46:14 +0200

    redstone seeds in listAllseed ore dic entry

--- src/main/java/de/pilz/sammelsorium/OreDictionaryManagement.java
+++ src/main/java/de/pilz/sammelsorium/OreDictionaryManagement.java
@@ -6,6 +6,7 @@ public class OreDictionaryManagement {
 
     public static final String ORE_DYE_RED = "dyeRed";
     public static final String ORE_DUST_GLOWSTONE = "dustGlowstone";
+    public static final String ORE_LIST_ALLSEED = "listAllseed";
 
     public static final String ORE_SHARD_PRISMARINE = "shardPrismarine";
     public static final String ORE_CRYSTAL_PRISMARINE_RAW = "crystalPrismarineRaw";
@@ -16,6 +17,9 @@ public class OreDictionaryManagement {
             OreDictionary.registerOre(ORE_SHARD_PRISMARINE, RegisterManagement.itemStackPrismarineShard);
             OreDictionary.registerOre(ORE_CRYSTAL_PRISMARINE_RAW, RegisterManagement.itemStackPrismarineCrystalRaw);
             OreDictionary.registerOre(ORE_CRYSTAL_PRISMARINE, RegisterManagement.itemStackPrismarineCrystal);
+        }
+        if (Config.enableGlowFlower) {
+            OreDictionary.registerOre(ORE_LIST_ALLSEED, RegisterManagement.itemCropsRedstone);
         }
     }
 }
