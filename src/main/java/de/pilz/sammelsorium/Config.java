9bdf8e4963c6f9b1c834dd4f37538c0a4691412c
Pilzinsel64 <pilzinsel64@gmx.de>
2023-04-12 14:27:10 +0200

    add `autoLoadChunksOnTicketCreation`

--- src/main/java/de/pilz/sammelsorium/Config.java
+++ src/main/java/de/pilz/sammelsorium/Config.java
@@ -13,6 +13,7 @@ public class Config {
     public static Boolean enableRedstoneFlower = true;
     public static Boolean enableDecoBlocksAndItems = false;
     public static Boolean disableChunkLoadingOnRequest = false;
+    public static Boolean autoLoadChunksOnTicketCreation = true;
 
     public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
@@ -41,7 +42,12 @@ public class Config {
                 "disableChunkLoadingOnRequest",
                 CATEGORY_EXPERIMENTAL,
                 disableChunkLoadingOnRequest,
-                "Disables the option \"loadChunkOnProvideRequest \" on server to not load chunks whenever they are maybe requested. This is very experimental.");
+                "Disables the option \"loadChunkOnProvideRequest\" on server to not load chunks whenever they are maybe requested. This is very experimental.");
+        autoLoadChunksOnTicketCreation = configuration.getBoolean(
+                "autoLoadChunksOnTicketCreation",
+                CATEGORY_EXPERIMENTAL,
+                autoLoadChunksOnTicketCreation,
+                "When \"disableChunkLoadingOnRequest\" is true then this option helps that chunk load tickets continue to load the target chunk automatically. Otherwise such tickets are useless and chunkloaders too. You may want to disable this if your Chunkloader mod can that too (probably not).");
 
         if (configuration.hasChanged()) {
             configuration.save();
--- src/main/java/de/pilz/sammelsorium/EventHandlers.java
+++ src/main/java/de/pilz/sammelsorium/EventHandlers.java
@@ -1,7 +1,9 @@
 package de.pilz.sammelsorium;
 
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraftforge.common.ForgeChunkManager.ForceChunkEvent;
 import net.minecraftforge.event.world.WorldEvent;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +16,16 @@ public class EventHandlers {
             IChunkProvider chunkProvider = event.world.getChunkProvider();
             if (chunkProvider instanceof ChunkProviderServer) {
                 ((ChunkProviderServer) chunkProvider).loadChunkOnProvideRequest = false;
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onChunkForce(ForceChunkEvent event) {
+        if (Config.disableChunkLoadingOnRequest && Config.autoLoadChunksOnTicketCreation && !event.ticket.world.isRemote) {
+            if (event.ticket.world instanceof WorldServer) {
+                ((WorldServer) event.ticket.world).getChunkProvider()
+                        .loadChunk(event.location.chunkXPos, event.location.chunkZPos);
             }
         }
     }
