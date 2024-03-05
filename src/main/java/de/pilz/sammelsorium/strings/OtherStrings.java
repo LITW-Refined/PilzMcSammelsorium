e878592bcc6256702fbbf93023708a10e650e6ca
Pilzinsel64 <pilzinsel64@gmx.de>
2024-03-05 14:08:41 +0100

    fix OtherStrings

--- src/main/java/de/pilz/sammelsorium/strings/OtherStrings.java
+++ src/main/java/de/pilz/sammelsorium/strings/OtherStrings.java
@@ -1,8 +1,10 @@
 package de.pilz.sammelsorium.strings;
 
+import de.pilz.sammelsorium.PilzMcSammelsorium;
+
 public class OtherStrings {
 
-    public static final String MOD_PREFIX = "pilzmcsammelsorium:";
+    public static final String MOD_PREFIX = PilzMcSammelsorium.MODID;
     public static final String TILE_PREFIX = "tile.";
     public static final String ITEM_PREFIX = "item.";
     public static final String STAGE_POSTFIX = "_stage_";
