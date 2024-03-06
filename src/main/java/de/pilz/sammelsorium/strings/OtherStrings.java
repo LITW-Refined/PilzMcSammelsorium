42c1849dbd9809f9b27b0b830ff791029f8fc247
Pilzinsel64 <pilzinsel64@gmx.de>
2024-03-06 13:48:51 +0100

    fix textures not showing

--- src/main/java/de/pilz/sammelsorium/strings/OtherStrings.java
+++ src/main/java/de/pilz/sammelsorium/strings/OtherStrings.java
@@ -4,7 +4,7 @@ import de.pilz.sammelsorium.PilzMcSammelsorium;
 
 public class OtherStrings {
 
-    public static final String MOD_PREFIX = PilzMcSammelsorium.MODID;
+    public static final String MOD_PREFIX = PilzMcSammelsorium.MODID + ":";
     public static final String TILE_PREFIX = "tile.";
     public static final String ITEM_PREFIX = "item.";
     public static final String STAGE_POSTFIX = "_stage_";
